ae9f6dbc5fa8e4f38ffc8755481ae9aee6ce140b
EduardoSandovalZ <100109750+EduardoSandovalZ@users.noreply.github.com>
2022-03-13 00:17:41 -0600

    Add files via upload

---
+++ SJF.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+struct PCB
+{
+    int pid, burst, wait, turnaround;
+};
+void pline(int);
+void pline(int x)
+{
+    int i;
+    for (i = 0; i < x; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+}
+int main()
+{
+    struct PCB p[10], temp;
+    int i, j, n, sum = 0, w_total = 0, t_total = 0;
+    float w_avg = 0, t_avg = 0.0;
+    cout << endl
+         << "Bienvenido al programa de Shortest Job First" << endl;
+    cout << "Ingrese el total de procesos :";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Ingrese la duracion del proceso " << i + i << ": ";
+        cin >> p[i].burst;
+        p[i].pid = i + 1;
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; i < n - i; j++)
+        {
+            if (p[j].burst > p[j + 1].burst)
+            {
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        p[i].wait = sum;
+        sum += p[i].burst;
+        p[i].turnaround = sum;
+    }
+    pline(35);
+    // cout de lo que se va a imprimir pero por el teclado no puedo poner la barra invertida
+    pline(35);
+    for (i = 0; i < n; i++)
+    {
+        // cout de lo que se va a imprimir pero por el teclado no puedo poner la barra invertida
+        w_total += p[i].wait;
+        t_total += p[i].turnaround;
+    }
+    w_avg = w_total / (float)n;
+    t_avg = t_total / (float)n;
+    cout << endl
+         << "Tiempo total de espera" << w_total << endl;
+    cout << "Tiempo promedio de espera" << w_avg << endl;
+    cout << "Tiempo total de respuesta " << t_total << endl;
+    cout << "Tiempo promedio de respuesta" << t_avg << endl;
+    return 0;
+}
