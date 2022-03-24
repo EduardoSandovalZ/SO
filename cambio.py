a31a0ce15415bf2c4c97cb00763a6809a575b155
EduardoSandovalZ <100109750+EduardoSandovalZ@users.noreply.github.com>
2022-03-24 16:51:56 -0600

    Update cambio.py

--- cambio.py
+++ cambio.py
@@ -1,3 +1,4 @@
+from sys import stderr
 def cambio (cambio,tipo):
     billetes=cantidad //tipo
     resto=cantidad % tipo
@@ -13,5 +14,15 @@ try:
     mon5, resto =cambio(resto,5)
     mon1, resto =cambio(resto,1)
 
-if mon100>0:
-    print('(0)')
+    if mon100>0:
+        print('(0) billetes de 100.'.format(mon100))
+    if mon25>0:
+        print('(0) billetes de 25.'.format(mon25))
+    if mon10>0:
+        print('(0) billetes de 10.'.format(mon10))
+    if mon5>0:
+        print('(0) billetes de 5.'.format(mon5))
+    if mon1>0:
+        print('(0) billetes de 1.'.format(mon1))
+except ValueError as e:
+    print('El valor introducido no es un entero.',file=stderr)
